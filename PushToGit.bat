e065528e7cb6b34e6e60b4f1cb80b526e32c6550
November21st <92364715+November21st@users.noreply.github.com>
2023-04-03 18:06:37 +0800

    bat???????:2023/04/03,18:06:37

---
+++ PushToGit.bat
@@ -0,0 +1,19 @@
+@echo off
+
+:: ?????????
+cd /d %~dp0
+:: ????
+git init 
+git add . 
+ git commit -m "bat???????:%date:~0,10%,%time:~0,8%" 
+::  git commit -m "%commitMessage%" 
+git push origin master
+@echo ????,
+
+SET daoTime=60
+:dao
+set /a daoTime=daoTime-1
+ping -n 2 -w 500 127.1>nul
+cls
+echo ??Git?? ,?????: %daoTime%?
+if %daoTime%==0 (exit) else (goto dao)
