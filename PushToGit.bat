0f7e6dbe4deca89f5e9520479386460e15ae8245
November21st <92364715+November21st@users.noreply.github.com>
2023-04-03 18:18:55 +0800

    bat批处理自动推送:2023/04/03,18:18:55

--- PushToGit.bat
+++ PushToGit.bat
@@ -1,20 +1,23 @@
 @echo off
 
-:: ?????????
+:: 获取当前脚本的路径
 cd /d %~dp0
-:: ????
+:: 自动提交
 git init 
+@echo 同步远端资源
 git pull
+@echo 更新远端资源
 git add . 
- git commit -m "bat???????:%date:~0,10%,%time:~0,8%" 
+ git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push origin master
-@echo ????,
+@echo 已经完成,
 
-SET daoTime=60
-:dao
-set /a daoTime=daoTime-1
-ping -n 2 -w 500 127.1>nul
-cls
-echo ??Git?? ,?????: %daoTime%?
-if %daoTime%==0 (exit) else (goto dao)
+echo;
+echo 批处理执行完毕！
+echo;
+
+pause
+
+
+
